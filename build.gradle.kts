f09481b8b3f3f8f58ac58c880ece8d30c5cfa8fb
selimdawa <selimdawa@gmail.com>
2026-05-12 01:36:47 +0300

    Nav Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
     repositories {
         google()
     }
-    val nav_version = "2.5.3"
+    val nav_version = "2.9.8"
 
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
@@ -16,5 +16,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
 
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.8" apply false
 }
