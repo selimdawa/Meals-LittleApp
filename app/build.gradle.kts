0513333625d4d8b7c8c708180835b6f89886bc83
selimdawa <selimdawa@gmail.com>
2026-05-13 04:19:50 +0300

    Nav Toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.navigation.safeargs.kotlin)
     id("com.google.devtools.ksp")
-    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-
-    repositories {
-        google()
-    }
-    val nav_version = "2.9.8"
-
-    dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-
+    alias(libs.plugins.navigation.safeargs.kotlin) apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.9.8" apply false
 }
