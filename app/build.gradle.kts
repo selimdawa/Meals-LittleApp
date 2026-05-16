d882899ce9b8d4475db1948334a7ebc1c29138f6
selimdawa <selimdawa@gmail.com>
2026-05-16 03:41:25 +0300

    Gradle 9.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.navigation.safeargs.kotlin)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp.processor)
 }
 
