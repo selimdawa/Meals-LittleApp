294547d5f1aa2ef5c6cdaf875392f2647d4a00cc
selimdawa <selimdawa@gmail.com>
2026-06-09 03:37:48 +0300

    SDK 37 / Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,15 +8,13 @@ plugins {
 android {
     namespace = "com.littleapp.meals"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.littleapp.meals"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
