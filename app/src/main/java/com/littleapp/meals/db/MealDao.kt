7e3a83a497f4a43fb6fda76ae3b9de6d1f229973
selimdawa <selimdawa@gmail.com>
2026-06-23 23:38:08 +0300

    Warning Fix

--- app/src/main/java/com/littleapp/meals/db/MealDao.kt
+++ app/src/main/java/com/littleapp/meals/db/MealDao.kt
@@ -1,12 +1,12 @@
 package com.littleapp.meals.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.littleapp.meals.pojo.Meal
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MealDao {
@@ -18,5 +18,5 @@ interface MealDao {
     suspend fun delete(meal: Meal)
 
     @Query("SELECT * FROM mealInformation")
-    fun getAllMeals(): Flow<List<Meal>>
+    fun getAllMeals(): LiveData<List<Meal>>
 }
